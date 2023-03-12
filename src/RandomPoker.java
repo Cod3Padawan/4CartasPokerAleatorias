7a81a902251f944e0d788a1c0ec0f2076f542172
Ji156 <jo.izquierdo@aulanz.net>
2023-03-12 11:37:12 +0100

    Primer commit

---
+++ src/RandomPoker.java
@@ -0,0 +1,175 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.RoundRectangle2D;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class RandomPoker {
+    private static final int IMAGE_WIDTH = 200;
+    private static final int IMAGE_HEIGHT = 300;
+    private static final Color POKER_GREEN = new Color(40, 104, 51);
+    private static final Color BACKGROUND_GREEN = new Color(39, 174, 96);
+
+    private List<File> imageFiles;
+    private JPanel panel;
+
+    private JLabel counterLabel;
+    private int counter;
+    private int numClicks;
+
+
+    public class RoundedImageLabel extends JLabel {
+        private static final long serialVersionUID = 1L;
+
+        private int borderRadius;
+
+        public RoundedImageLabel(ImageIcon icon, int borderRadius) {
+            super(icon);
+            this.borderRadius = borderRadius;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            RoundRectangle2D.Float border = new RoundRectangle2D.Float(0, 0, getWidth(), getHeight(), borderRadius, borderRadius);
+            g2.clip(border);
+            super.paintComponent(g2);
+        }
+    }
+
+    public static void main(String[] args) {
+        String folderPath = "//home/ji156mint/IdeaProjects/RandomPoker/src/cartas";
+        RandomPoker randomPoker = new RandomPoker();
+        randomPoker.imageFiles = randomPoker.loadImageFiles(folderPath);
+        Collections.shuffle(randomPoker.imageFiles);
+
+        JFrame frame = new JFrame("Random Poker Card");
+        frame.getContentPane().setBackground(BACKGROUND_GREEN);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        randomPoker.panel = randomPoker.createPanel();
+        frame.add(randomPoker.panel);
+        randomPoker.counter = 0;
+        randomPoker.counterLabel = new JLabel("Pulsaciones: " + randomPoker.numClicks);
+        randomPoker.counterLabel.setHorizontalAlignment(JLabel.CENTER);
+        randomPoker.counterLabel.setVerticalAlignment(JLabel.CENTER);
+        randomPoker.counterLabel.setFont(randomPoker.counterLabel.getFont().deriveFont(24f));
+        frame.add(randomPoker.counterLabel, "North");
+
+        JButton generateButton = new JButton("Generar nuevas cartas");//Con esto generamos nuevas cartas y cada click se suma al marcador
+        generateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                randomPoker.updatePanel();
+                randomPoker.numClicks++;
+                randomPoker.counterLabel.setText("Pulsaciones: " + randomPoker.numClicks);
+            }
+        });
+
+
+        JButton resetButton = new JButton("Resetear contador");
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                randomPoker.numClicks = 0; // resetea el contador de pulsaciones
+                randomPoker.counterLabel.setText("Pulsaciones: " + randomPoker.numClicks); // actualiza la etiqueta del contador de pulsaciones
+            }
+        });
+
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBackground(BACKGROUND_GREEN);
+        buttonPanel.add(generateButton);
+        buttonPanel.add(resetButton);
+        frame.add(buttonPanel, "South");
+
+        frame.setPreferredSize(new Dimension(800, 800));
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private List<File> loadImageFiles(String folderPath) {
+        Objects.requireNonNull(folderPath, "La ruta de la carpeta no puede ser nula");
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles();
+        List<File> imageFiles = new ArrayList<>();
+
+        if (files != null) {
+            for (File file : files) {
+                String fileName = file.getName().toLowerCase();
+                if (fileName.endsWith(".png") && !fileName.startsWith("volver")) {
+                    imageFiles.add(file);
+                }
+            }
+        }
+        return imageFiles;
+    }
+
+    private JPanel createPanel() {
+        JPanel panel = new JPanel();
+        panel.setBackground(POKER_GREEN);
+        panel.setLayout(new GridLayout(2, 2));
+
+        for (int i = 0; i < 4; i++) {
+            File imageFile = imageFiles.get(i % imageFiles.size());
+            ImageIcon imageIcon = loadImageIcon(imageFile);
+            JLabel label = new RoundedImageLabel(imageIcon, 5); // Radio de borde de 20px
+            panel.add(label);
+        }
+
+        return panel;
+    }
+
+    private void updatePanel() {
+        panel.removeAll();
+        Collections.shuffle(imageFiles);
+
+        for (int i = 0; i < 4; i++) {
+            File imageFile = imageFiles.get(i % imageFiles.size());
+            ImageIcon imageIcon = loadImageIcon(imageFile);
+            JLabel label = new JLabel(imageIcon);
+            panel.add(label);
+        }
+
+        panel.revalidate();
+        panel.repaint();
+    }
+
+    private void resetCounter() {
+        int counter = 0;
+        Label counterLabel = null;
+        counterLabel.setText("Número de pulsaciones: " + counter);
+    }
+
+
+
+    private ImageIcon loadImageIcon(File imageFile) {
+        try {
+            return new ImageIcon(ImageIO.read(imageFile).getScaledInstance(
+                    IMAGE_WIDTH, IMAGE_HEIGHT, java.awt.Image.SCALE_SMOOTH));
+        } catch (IOException e) {
+            System.err.println("Error al leer archivo de imagen: " + imageFile);
+            return null;
+        }
+    }
+}
